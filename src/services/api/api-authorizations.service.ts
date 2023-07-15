d863f8c3df15a59c3e8077479fb70458c541e403
PaulSaillant <paulsaillant@outlook.fr>
2023-07-15 17:53:08 +0200

    Return all authorizations

--- src/services/api/api-authorizations.service.ts
+++ src/services/api/api-authorizations.service.ts
@@ -5,19 +5,23 @@ import type { Authorization } from '$models/data/authorization.model'
 import type { DefaultHttpResponse } from '$models/responses/default-http-response.model'
 
 export default class ApiAuthorizationsService {
+  async getAuthorizations() {
+    return await http.get<DataHttpResponse<{ authorizations: Authorization[] }>>(`${api}/authorizations`)
+  }
+
   async getAuthorization(id: number | string) {
-    return await http.get<DataHttpResponse<{ authorization: Authorization }>>(`${api}/authorizations/${id}`)
+    return await http.get<DataHttpResponse<{ authorizations: Authorization[] }>>(`${api}/authorizations/${id}`)
   }
 
   async postAuthorization(authorization: Authorization) {
-    return await http.post<DataHttpResponse<{ authorization: Authorization }>>(`${api}/authorizations`, authorization)
+    return await http.post<DataHttpResponse<{ authorizations: Authorization[] }>>(`${api}/authorizations`, authorization)
   }
 
   async putAuthorization(authorization: Partial<Authorization>, id: number | string) {
-    return await http.put<DataHttpResponse<{ authorization: Authorization }>>(`${api}/authorizations/${id}`, authorization)
+    return await http.put<DataHttpResponse<{ authorizations: Authorization[] }>>(`${api}/authorizations/${id}`, authorization)
   }
 
   async deleteAuthorization(id: number | string) {
-    return await http.delete<DefaultHttpResponse>(`${api}/authorizations/${id}`)
+    return await http.delete<DataHttpResponse<{ authorizations: Authorization[] }>>(`${api}/authorizations/${id}`)
   }
 }
