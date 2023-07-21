cc87746ed97802717d5e4a9be48e4f0897b6cc48
PaulSaillant <paulsaillant@outlook.fr>
2023-07-21 20:38:21 +0200

    Update type.ts

--- src/params/type.ts
+++ src/params/type.ts
@@ -1,5 +1,4 @@
 import type { ParamMatcher } from '@sveltejs/kit'
-import utils from '$services/utils'
 
 export const match: ParamMatcher = (param) => {
   return ['emissions', 'videos', 'articles'].includes(param)
